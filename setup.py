8a248ec2f13b11c41b435ed1a7cbe013b4d9f81b
Matthew Feickert <matthew.feickert@cern.ch>
2021-11-16 17:50:01 -0600

    include_package_data

--- setup.py
+++ setup.py
@@ -171,6 +171,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
+    include_package_data=True,
 
     # optional pip dependencies
     extras_require={
